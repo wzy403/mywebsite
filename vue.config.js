9ddb6e8814e94fcd71f750a95e3445b81ce32e2d
Zhengyu-Joey-Wang <XDwoshiyingyan@gmail.com>
2024-06-10 21:27:51 -0400

    add backend proxy target

--- vue.config.js
+++ vue.config.js
@@ -2,3 +2,14 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true
 })
+module.exports = {
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8081',
+        changeOrigin: true
+      }
+    }
+  }
+}
+
