92d2f8d3ad47ac2da989aa90bd8d1869da8eba4b
Zhengyu-Joey-Wang <XDwoshiyingyan@gmail.com>
2024-07-23 21:54:01 -0400

    fix scrolling problem

--- src/App.vue
+++ src/App.vue
@@ -16,11 +16,6 @@ export default {
 </script>
 
 <style>
-body {
-  background-color: #242424;
-  margin: 0;
-  overflow: hidden;
-}
 #app {
   font-family: "Comic Sans MS", "Helvetica Neue", "Microsoft Yahei",
     "-apple-system", "sans-serif";
@@ -33,29 +28,5 @@ body {
   flex-direction: column;
   justify-content: flex-start;
   vertical-align: middle;
-}
-
-/* 设置浏览器滚动条样式 */
-/* 浏览器滚动条样式 */
-/* width */
-::-webkit-scrollbar {
-  width: 3px;
-  height: 3px;
-}
-/* Track */
-::-webkit-scrollbar-track {
-  background: #f3f3f3;
-  border-radius: 8px;
-}
-/* Handle */
-::-webkit-scrollbar-thumb {
-  background: rgba(201, 201, 202, 0.5);
-  border-radius: 8px;
-}
-/* Handle on hover */
-::-webkit-scrollbar-thumb:hover {
-  /* background: rgb(162, 162, 163); */
-  background: rgba(201, 201, 202, 0.5);
-  border-radius: 8px;
 }
 </style>
