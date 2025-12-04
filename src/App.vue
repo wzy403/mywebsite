f3caa094e8ffd041d72d63fef7943fc22c7ab2a4
wzy403 <XDwoshiyingyan@gmail.com>
2025-12-04 16:40:35 -0500

    Fix font display issue on macOS

--- src/App.vue
+++ src/App.vue
@@ -18,9 +18,25 @@ export default {
 <style>
 #app {
   font-family: "Comic Sans MS", "Helvetica Neue", "Microsoft Yahei",
-    "-apple-system", "sans-serif";
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+    "Segoe UI",
+    "Roboto",
+    "Helvetica Neue",
+    "PingFang SC",
+    "Hiragino Sans GB",
+    sans-serif; 
+
+  /* font-family:
+    -apple-system,
+    BlinkMacSystemFont,
+    "Segoe UI",
+    "Roboto",
+    "Helvetica Neue",
+    "PingFang SC",
+    "Hiragino Sans GB",
+    "Microsoft YaHei",
+    sans-serif;   */
+  /* -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale; */
   /* text-align: center; */
   /* color: #ffff; */
   /* margin-top: 25vh; */
